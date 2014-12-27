3b8bb740a10d8c5ee170c8c756e7f2a93d30dcd8
aironoliveira <airon_oliveira@hotmail.com>
2014-12-27 16:32:59 -0200

    Inserindo Leitura e escrita em arquivo

--- app/src/main/java/com/oliveira/airon/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/oliveira/airon/myfirstapplication/MainActivity.java
@@ -1,12 +1,20 @@
 package com.oliveira.airon.myfirstapplication;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -41,13 +49,52 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /* called when the user clicks the Send button */
-    public void sendMessage(View view){
+    public void sendMessage(View view) throws IOException {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
 
         String message = editText.getText().toString();
 
-        intent.putExtra(EXTRA_MESSAGE, message);
+        saveText(message);
+        //intent.putExtra(EXTRA_MESSAGE, message);
+
+        intent.putExtra(EXTRA_MESSAGE, getSavedText(getString(R.string.file_name))+" \nTexto Obtido de arquivo salvo");
         startActivity(intent);
+    }
+
+    public void saveText (String string){
+        FileOutputStream outputStream;
+
+        try{
+            outputStream = openFileOutput(getString(R.string.file_name), Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getSavedText(String fileLocation) {
+        try{
+            InputStream in = openFileInput(fileLocation);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder str = new StringBuilder();
+            String line;
+            while((line = reader.readLine()) != null)
+            {
+                str.append(line);
+            }
+            in.close();
+            return str.toString();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+            return "";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+
     }
 }
